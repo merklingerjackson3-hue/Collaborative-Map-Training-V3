eb07a1aced067427c9e517608e4692ae138c0412
Jackson Merklinger <merklingerjackson3@gmail.com>
2026-08-29 22:55:01 +0900

    V3 foundation checkpoint

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+const PORT = process.env.PORT || 3002;
+
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+    console.log("User connected:", socket.id);
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected:", socket.id);
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`V3 server running on port ${PORT}`);
+});
